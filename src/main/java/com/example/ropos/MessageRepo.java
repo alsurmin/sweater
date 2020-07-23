12a6261bee8909acadb8f22526368d1fef33d7fd
alex <amsurmin@gmail.com>
2020-07-23 23:23:42 +0300

    Turbolinks

--- src/main/java/com/example/ropos/MessageRepo.java
+++ src/main/java/com/example/ropos/MessageRepo.java
@@ -16,18 +16,18 @@ import org.springframework.data.repository.query.Param;
 public interface MessageRepo extends CrudRepository<Message, Long> {
 
     @Query("select new com.example.domain.dto.MessageDto(" +
-            "m, " +
-            "count(ml), " +
-            "sum (case when ml = :user then 1 else 0 end) > 0" +
+            "   m, " +
+            "   count(ml), " +
+            "   sum(case when ml = :user then 1 else 0 end) > 0" +
             ") " +
             "from Message m left join m.likes ml " +
             "group by m")
     Page<MessageDto> findAll(Pageable pageable, @Param("user") User user);
 
     @Query("select new com.example.domain.dto.MessageDto(" +
-            "m, " +
-            "count(ml), " +
-            "sum (case when ml = :user then 1 else 0 end) > 0" +
+            "   m, " +
+            "   count(ml), " +
+            "   sum(case when ml = :user then 1 else 0 end) > 0" +
             ") " +
             "from Message m left join m.likes ml " +
             "where m.tag = :tag " +
@@ -35,9 +35,9 @@ public interface MessageRepo extends CrudRepository<Message, Long> {
     Page<MessageDto> findByTag(@Param("tag") String tag, Pageable pageable, @Param("user") User user);
 
     @Query("select new com.example.domain.dto.MessageDto(" +
-            "m, " +
-            "count(ml), " +
-            "sum (case when ml = :user then 1 else 0 end) > 0" +
+            "   m, " +
+            "   count(ml), " +
+            "   sum(case when ml = :user then 1 else 0 end) > 0" +
             ") " +
             "from Message m left join m.likes ml " +
             "where m.author = :author " +
